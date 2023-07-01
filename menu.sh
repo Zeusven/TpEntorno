bc75e17007132cdf56e2493639c5c1d2a80b16cd
Fefe <ftroanes@gmail.com>
2023-07-01 11:38:26 -0300

    Creacion del menu. Archivo "descargar" renombrado a "descomprimir"

---
+++ menu.sh
@@ -0,0 +1,28 @@
+PS3="Elija una tarea : "
+OPCIONES=("Generar imágenes" "Descomprimir imágenes" "Procesar imágenes" "Comprimir imágenes" "Salir")
+select OPT in "${OPCIONES[@]}"
+do
+  case $OPT in
+    "Generar imágenes")
+      bash generar.sh
+      break
+      ;;
+    "Descomprimir imágenes")
+      bash descomprimir.sh
+      break
+      ;;
+    "Procesar imágenes")
+      bash procesar.sh
+      break
+      ;;
+    "Comprimir imágenes")
+      bash comprimir.sh
+      break
+      ;;
+    "Salir")
+      break 2
+      ;;
+    *)
+      echo "Opcion no válida."
+  esac
+done
