cce461d0884b685c4f121321aec482344ad3fba7
FefeTroanes <ftroanes@gmail.com>
2023-08-07 21:34:12 -0300

    feat generar descarga de imagenes y asignacion de nombre al azar
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

--- scripts/generar.sh
+++ scripts/generar.sh
@@ -4,14 +4,14 @@
 # en cuenta que al descargar de una página conviene usar un sleep entre
 # descarga y descarga para no saturar el servicio y evitar problemas. Se debe
 # poder indicar por argumento cuantas imágenes generar
+# y se deben asignar nombres de archivo al azar de una lista de nombres de personas.
 
-# y se deben asignar nombres de archivo al azar de una lista de nombres de personas. Luego
-# se deben comprimir las imágenes, y generar un archivo con su suma de
-# verificación.
+# Luego se deben comprimir las imágenes
+# y generar un archivo con su suma de verificación.
 
 echo "Cuantas imagenes desea generar?"
 read CANTIDAD_IMAGENES
-
+RUTA_CSV="../datasets/names.csv"
 
 # Limpieza del directorio y descarga del csv con nombres
 if [ $"ls ../datasets/*.jpg" ]
@@ -23,39 +23,17 @@ then
     echo "El csv ya existe."
   else
     echo "Descargando csv"
-    curl https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv -o ../datasets/names.csv
+    curl https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv -o $RUTA_CSV
   fi
 fi
 
-function descargar_csv() {
-  RUTA_CSV="../datasets/names.csv"
-  # NOMBRES=$(cat "$RUTA_CSV")
-  # LISTA_NOMBRES=()
-  while IFS= read -r LINE
-  do
-    NOMBRE=$(echo "$LINE" | awk -F ',' '{print $1}')  # Extraer el nombre antes de la coma
-    echo $NOMBRE
-    LISTA_NOMBRES+=("$NOMBRE")
-  done < "$RUTA_CSV"
-}
+LISTA_NOMBRES=$(cat $RUTA_CSV | awk -F',' '{print $1}')
 
-descargar_csv
-
-echo "antes del for"
-for asd in "${LISTA_NOMBRES[@]}"
+# Descarga de imagenes y asignacion de nombre random
+for (( i=1; i<=CANTIDAD_IMAGENES; i+=1 ))
 do
-  echo "$asd"
+  NOMBRE_ALEATORIO=$(shuf -n 1 <<< "$LISTA_NOMBRES")
+  curl https://thispersondoesnotexist.com/ -o "../datasets/$NOMBRE_ALEATORIO.jpg"
+  sleep 1
 done
-echo "despues del for"
-
-
-# Descarga de imagenes
-# for (( i=1; i<=CANTIDAD_IMAGENES; i+=1 ))
-# do
-#   curl https://thispersondoesnotexist.com/ -o ../datasets/imagen$i.jpg
-#   sleep 1
-# done
-
-# echo $NOMBRES
-
 
