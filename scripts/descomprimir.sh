a699dcb1eea5e6974682b26fc9b5e2aac8028852
FefeTroanes <ftroanes@gmail.com>
2023-08-08 11:55:46 -0300

    feat descomprimir: Solicitud de archivos, verificacion y descompresion
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

--- scripts/descomprimir.sh
+++ scripts/descomprimir.sh
@@ -1,3 +1,49 @@
 #!/bin/bash
 
-echo "archivo descomprimir"
+# Se debe poder indicar por argumento dos archivos (uno con las imágenes comprimidas y otro con una suma de verificación).
+# Si ocurrió algún error se debe informar al usuario
+# de lo contrario se procede a descomprimir.
+
+echo "Ingrese el nombre del archivo que desea descomprimir: "
+echo "Recuerde que las extensiones son <nombre>.tar.gz"
+read ARCHIVO_DESCOMPRIMIR
+
+echo "Ingrese el nombre del archivo con la suma de verificacion: "
+echo "Recuerde que las extensiones son <nombre>.tar.gz.sha256"
+read ARCHIVO_VERIFICACION
+
+# Para hacer pruebas
+ARCHIVO_DESCOMPRIMIR="$ARCHIVO_DESCOMPRIMIR.tar.gz"
+ARCHIVO_VERIFICACION="$ARCHIVO_VERIFICACION.tar.gz.sha256"
+
+# Busqueda de archivos
+FLAG=0
+if [ -f "../datasets/$ARCHIVO_DESCOMPRIMIR" ]
+then
+  echo "tar.gz encontrado"
+else
+  echo "tar.gz inexistente"
+  (( FLAG+=1 ))
+fi
+
+if [ -f "../datasets/$ARCHIVO_VERIFICACION" ]
+then
+  echo "tar.gz.sha256 encontrado"
+else
+  echo "tar.gz.sha256 inexistente"
+  (( FLAG+=1 ))
+fi
+
+# Control de verificacion
+PARA_VERIFICAR1=$(sha256sum ../datasets/$ARCHIVO_DESCOMPRIMIR)
+PARA_VERIFICAR2=$(cat ../datasets/$ARCHIVO_VERIFICACION)
+
+if [ "$PARA_VERIFICAR1" == "$PARA_VERIFICAR2" ]
+then
+  echo "son iguales"
+  mkdir -p ../datasets/descomprimido
+  tar -zxvf ../datasets/$ARCHIVO_DESCOMPRIMIR -C ../datasets/descomprimido
+else
+  echo "no son iguales"
+fi
+
