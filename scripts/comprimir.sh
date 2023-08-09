e49eea7dd58271df9ac71f91e26706c6a378ccb4
FefeTroanes <ftroanes@gmail.com>
2023-08-09 11:38:33 -0300

    feat comprimir: se disenio la compresion de todo lo solicitado y se movieron algunas cositas de un script al otro para no repetir codigo
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

--- scripts/comprimir.sh
+++ scripts/comprimir.sh
@@ -7,29 +7,20 @@
 #   la letra a.
 
 # - por último, generar un archivo comprimido que incluya los archivos
-# generados en los items anteriores y todas las imágenes. El archivo
-# comprimido debe poder accederse desde fuera del contenedor.
+#   generados en los items anteriores y todas las imágenes. El archivo
+#   comprimido debe poder accederse desde fuera del contenedor.
 
-RUTA_IMAGENES="../datasets/imagenes"
-LISTA_IMAGENES=("$RUTA_IMAGENES"/*)
-# mkdir -p ../datasets/procesado
-mkdir -p ../datasets/para_comprimir
+# function comprimir() {
+#   # El primer parametro es el nombre del archivo que se crea
+#   # El segundo es la ruta del archivo a comprimir
+#   tar -zcvf $1 "$2"
+# }
 
+mv ../datasets/imagenes ../datasets/para_comprimir/
+mv ../datasets/procesado ../datasets/para_comprimir/
 
+tar -zcvf ../datasets/resultado.tar.gz "../datasets/para_comprimir"
 
-for IMAGEN in "${LISTA_IMAGENES[@]}"
-do
-  NOMBRE_ARCHIVO=$(basename "$IMAGEN")
-  echo $NOMBRE_ARCHIVO >> ../datasets/para_comprimir/todas_las_imagenes.txt
-  if [[ $NOMBRE_ARCHIVO =~ ^[A-Z][a-z]* ]]
-  then
-    echo $NOMBRE_ARCHIVO >> ../datasets/para_comprimir/imagenes_validas.txt
-    # convert "$RUTA_IMAGENES/$NOMBRE_ARCHIVO" -gravity center -resize 512x512+0+0 -extent 512x512 "../datasets/procesado/$NOMBRE_ARCHIVO"
-  fi
-  if [[ $NOMBRE_ARCHIVO =~ a\.jpg$ ]]
-  then
-    echo $NOMBRE_ARCHIVO >> ../datasets/para_comprimir/terminadas_en_a.txt
-    # convert "$RUTA_IMAGENES/$NOMBRE_ARCHIVO" -gravity center -resize 512x512+0+0 -extent 512x512 "../datasets/procesado/$NOMBRE_ARCHIVO"
-  fi
-done
-
+# # para probar
+# mkdir -p ../datasets/resultado
+# tar -zxvf ../datasets/resultado.tar.gz -C ../datasets/resultado
--- scripts/procesar.sh
+++ scripts/procesar.sh
@@ -8,10 +8,15 @@ mkdir -p ../datasets/para_comprimir
 for IMAGEN in "${LISTA_IMAGENES[@]}"
 do
     NOMBRE_ARCHIVO=$(basename "$IMAGEN")
-    # Verificar si el nombre de archivo empieza con mayuscula y sigue con minusculas
+    echo $NOMBRE_ARCHIVO >> ../datasets/para_comprimir/todas_las_imagenes.txt
+
     if [[ $NOMBRE_ARCHIVO =~ ^[A-Z][a-z]* ]]
     then
         echo $NOMBRE_ARCHIVO >> ../datasets/para_comprimir/imagenes_validas.txt
         convert "$RUTA_IMAGENES/$NOMBRE_ARCHIVO" -gravity center -resize 512x512+0+0 -extent 512x512 "../datasets/procesado/$NOMBRE_ARCHIVO"
+    fi
+    if [[ $NOMBRE_ARCHIVO =~ a\.jpg$ ]]
+    then
+        echo $NOMBRE_ARCHIVO >> ../datasets/para_comprimir/terminadas_en_a.txt
     fi
 done
